be08e10dfa87aa6bcf6eb1c1cc65dde5605250d6
Luiz Felipe Vieira <1037880@sga.pucminas.br>
2023-04-12 22:08:35 -0300

    testes iniciais

---
+++ codigo/pucflix/src/ClienteTest.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class ClienteTest {
+    Cliente novoCliente;
+
+    @Test
+    @BeforeEach
+    public void init() {
+
+        novoCliente = new Cliente("Cliente Teste");
+
+    }
+
+    @Test
+    public void adicionarSerieNaListaParaVer(){
+        int tamanhoDaListaParaVer = getListaParaVer().size();
+        Serie s = new Serie("Bla Bla Bla");
+        novoCliente.adicionarNaLista(s.getId());
+        assertEquals(tamanhoDaListaParaVer+1, novoCliente.getListaParaVer);
+    }
+
+}
