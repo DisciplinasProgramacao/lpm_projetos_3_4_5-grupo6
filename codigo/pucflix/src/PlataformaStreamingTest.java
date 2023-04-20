bf9fb29887eb67ae5f7832b42ace1a05da4658ca
Paulo Roncarati <roncarati1209@gmail.com>
2023-04-20 00:03:11 -0300

    Add metodos vazios PlataformaStreamingTest

---
+++ codigo/pucflix/src/PlataformaStreamingTest.java
@@ -0,0 +1,46 @@
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public class PlataformaStreamingTest {
+	@BeforeAll
+	public void initAll() {
+		// TODO
+	}
+
+	@BeforeEach
+	public void initEach() {
+		// TODO
+	}
+
+	@Test
+	public void clienteDeveConseguirLogarComSucesso() {
+		// TODO
+	}
+
+	@Test
+	public void soDeveSerPossivelLogarSeEstiverCadastrado() {
+		// TODO
+	}
+
+	@Test
+	public void deveSerPossivelAdicionarSeriesAPlataforma() {
+		// TODO
+	}
+
+	@Test
+	public void naoDeveSerPossivelAdicionarUmaSerieDuplicada() {
+		// TODO
+	}
+
+	@Test
+	public void deveSerPossivelAdicionarUmNovoCliente() {
+		// TODO
+	}
+
+	@Test
+	public void naoEPermitidoAdicionarClienteComNomeDeUsuarioNulo() {
+		// TODO
+	}
+
+}
