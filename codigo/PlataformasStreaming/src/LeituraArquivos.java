daad8eeb9fa663eeac503804f5d2958398793224
Paulo Roncarati <roncarati1209@gmail.com>
2023-04-23 17:34:26 -0300

    Tratando caminho do arquivo p ambientes diferentes

--- codigo/PlataformasStreaming/src/LeituraArquivos.java
+++ codigo/PlataformasStreaming/src/LeituraArquivos.java
@@ -1,24 +1,43 @@
-import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 final class LeituraArquivos {
-	static ArrayList<Cliente> carregarClientes() throws FileNotFoundException {
+	static ArrayList<Cliente> carregarClientes() {
 		ArrayList<Cliente> clientes = new ArrayList<Cliente>();
-		Scanner scanner = new Scanner(new File("assets/Espectadores.csv"));
-		while (scanner.hasNext())
-			clientes.add(new Cliente(scanner.nextLine()));
-		scanner.close();
+		Scanner scanner;
+		try {
+			scanner = new Scanner(caminho("Espectadores.csv").toFile());
+			while (scanner.hasNext())
+				clientes.add(new Cliente(scanner.nextLine()));
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		}
 		return clientes;
 	}
 
-	static ArrayList<Serie> carregarSeries() throws FileNotFoundException {
+	static ArrayList<Serie> carregarSeries() {
 		ArrayList<Serie> series = new ArrayList<Serie>();
-		Scanner scanner = new Scanner(new File("assets/Series.csv"));
-		while (scanner.hasNext())
-			series.add(new Serie(scanner.nextLine()));
-		scanner.close();
+		Scanner scanner;
+		try {
+			scanner = new Scanner(caminho("Series.csv").toFile());
+			while (scanner.hasNext())
+				series.add(new Serie(scanner.nextLine()));
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		}
 		return series;
+	}
+
+	private static Path caminho(String arquivo) {
+		ClassLoader classLoader = LeituraArquivos.class.getClassLoader();
+		Path raizClasse = Paths.get(classLoader.getResource("").getPath());
+		Path raizProjeto = raizClasse.getParent();
+		Path caminhoArquivo = Paths.get(raizProjeto.toString(), "assets", arquivo);
+		return caminhoArquivo;
 	}
 }
--- docs/instrucoes.md
+++ docs/instrucoes.md
@@ -2,4 +2,6 @@
 
 ## Arquivos de dados
 
-Os arquivos de dados para teste e inicialização do sistema estão em `/codigo/PlataformasStreaming/assets`
+Os arquivos de dados para teste e inicialização do sistema estão em `codigo/PlataformasStreaming/assets`
+
+Quaisquer arquivos adicionais para carregamento de dados ou para testes devem ser inseridos nesse diretório
