09f06b7d699c438fe848b4249a367c0fe7f422dd
Paulo Roncarati <roncarati1209@gmail.com>
2023-04-24 17:50:50 -0300

    Add classe Filme

---
+++ codigo/PlataformasStreaming/src/Filme.java
@@ -0,0 +1,7 @@
+public class Filme extends Midia {
+	public Filme(String id, String nome, String dataLancamento) {
+		super(id, nome, dataLancamento);
+		// TODO Auto-generated constructor stub
+	}
+
+}
