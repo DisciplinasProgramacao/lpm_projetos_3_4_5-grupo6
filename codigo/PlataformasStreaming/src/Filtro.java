fee11ca37d9357a723758717bb3a13751c51e79f
Felipe Vieira <luizfelipevieira01@gmail.com>
2023-05-03 21:54:28 -0300

    update backlog.md

--- codigo/PlataformasStreaming/src/Filtro.java
+++ codigo/PlataformasStreaming/src/Filtro.java
@@ -7,7 +7,8 @@ public class Filtro<T extends Comparable<T>> { // adiciona restrição ao tipo g
     private final Comparator<T> comparadorPadrao;
 
     public Filtro(List<T> listaOriginal, Comparator<T> comparador) {
-        this.dadosOriginais = new ArrayList<>(listaOriginal); // cria uma cópia da lista original para evitar alterações inesperadas
+        this.dadosOriginais = new ArrayList<>(listaOriginal); // cria uma cópia da lista original para evitar alterações
+                                                              // inesperadas
         this.comparadorPadrao = comparador;
     }
 
@@ -15,7 +16,8 @@ public class Filtro<T extends Comparable<T>> { // adiciona restrição ao tipo g
         this(listaOriginal, null); // chama o construtor com parâmetros completos, passando null para o comparador
     }
 
-    public List<T> filtrar(T elementoID){ // muda o tipo de retorno para List<T>, que é o tipo do objeto criado na função
+    public List<T> filtrar(T elementoID) { // muda o tipo de retorno para List<T>, que é o tipo do objeto criado na
+                                           // função
         List<T> listaFiltrada = new ArrayList<>();
 
         for (T t : dadosOriginais) {
