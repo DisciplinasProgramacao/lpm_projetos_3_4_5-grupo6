c30f0e12aa5741eb2fef0226144f9bf325bbd3cd
Felipe Vieira <luizfelipevieira01@gmail.com>
2023-05-23 14:10:45 -0300

    implementando filtro

--- codigo/src/FiltroMidia.java
+++ codigo/src/FiltroMidia.java
@@ -1,11 +1,32 @@
+
 import java.util.ArrayList;
 
-public class FiltroMidia <T extends Comparable<T>> {
-        
-        FiltroMidia(String valorReferencia, T list){
-                ArrayList<T> listaOriginal = list;
-                ArrayList<T> listaNova = new ArrayList<>();
+import java.util.List;
 
-              
+public class FiltroMidia<T> {
+
+        public List<Midia> filtroGenero(String genero1, List<Midia> lista) {
+                List<Midia> midiasFiltradas = new ArrayList<Midia>();
+
+                for (Midia midia : lista) {
+                        if (genero1.equals(midia.getGenero())) {
+                                midiasFiltradas.add(midia);
+                        }
+                }
+
+                return midiasFiltradas;
         }
+
+        public List<Midia> filtroGenero(String genero1, List<Midia> lista) {
+                List<Midia> midiasFiltradas = new ArrayList<Midia>();
+
+                for (Midia midia : lista) {
+                        if (genero1.equals(midia.getGenero())) {
+                                midiasFiltradas.add(midia);
+                        }
+                }
+
+                return midiasFiltradas;
+        }
+
 }
