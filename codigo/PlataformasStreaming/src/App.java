7f7edf2b9c2c25ee4bfcc6440e0fe88e0b0a9c5e
Eric Diniz <64018893+ericdiniz@users.noreply.github.com>
2023-06-19 21:28:21 -0300

    commit novo app

--- codigo/PlataformasStreaming/src/App.java
+++ codigo/PlataformasStreaming/src/App.java
@@ -20,26 +20,45 @@ public class App {
         scanner.nextLine();
     }
 
+    /**
+     * Limpa o console exibindo o programa em execução.
+     * Essa função limpa a tela do console, removendo todas as saídas anteriores.
+     * O método usado para limpar o console varia dependendo do sistema operacional.
+     * Atualmente, o suporte é fornecido para sistemas Windows e outros sistemas
+     * operacionais.
+     * Se ocorrer um erro durante a execução da limpeza do console, a mensagem de
+     * erro será exibida.
+     */
     public static void limparConsole() {
         try {
             if (System.getProperty("os.name").contains("Windows")) {
+                // Limpa o console no Windows
                 new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
             } else {
+                // Limpa o console em outros sistemas operacionais
                 System.out.print("\033[H\033[2J");
                 System.out.flush();
             }
         } catch (Exception e) {
+            // Exibe a mensagem de erro, caso ocorra algum problema
             System.out.println(e.getMessage());
         }
     }
+
     // #endregion
 
+    /**
+     * Exibe o menu principal e retorna a opção selecionada pelo usuário.
+     *
+     * @return A opção selecionada pelo usuário.
+     */
     public static int menuPrincipal() {
         int opcao;
+
         System.out.println("==========================");
         System.out.println("MENU PRINCIPAL");
-        // relatorios
         System.out.println("0 - Cancelar" + System.lineSeparator());
+
         System.out.print("Digite sua opção: ");
         try {
             opcao = Integer.parseInt(scanner.nextLine());
@@ -513,7 +532,6 @@ public class App {
         } catch (IOException e) {
             System.out.println("Tente novamente, erro ao cadastrar o filme.");
             System.out.println("Problema encontrado: " + e.getMessage());
-
             cadastrarFilme();
         }
         System.out.println("Filme cadastrado com sucesso!");
