912645bd100249206be7ded7067a0478a81a6e0c
Felipe Vieira <luizfelipevieira01@gmail.com>
2023-06-19 21:25:37 -0300

    implementando teste da classe avaliacao

--- codigo/PlataformasStreaming/src/MidiaTest.java
+++ codigo/PlataformasStreaming/src/MidiaTest.java
@@ -1,18 +1,20 @@
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.Assert;
 import org.junit.Test;
 
+import Exceptions.SenhaFracaException;
 
 public class MidiaTest {
 
-
-
     @Test
     public void testCriarMidia() {
-        String   nome = "Filme A";
-        String  idioma = "Português";
-        String  genero = "Ação";
-        Double  duracao = 60d;
+        String nome = "Filme A";
+        String idioma = "Português";
+        String genero = "Ação";
+        Double duracao = 60d;
         Midia midia = new Filme(nome, idioma, genero, duracao);
         Assert.assertEquals("Filme A", midia.getNome());
         Assert.assertEquals(idioma, midia.getIdioma());
@@ -21,16 +23,35 @@ public class MidiaTest {
 
     @Test
     public void testCriarMidiaNomeNulo() {
-        String   nome = null;
-        String  idioma = "Português";
-        String  genero = "Ação";
-        Double  duracao = 60d;
-       
+        String nome = null;
+        String idioma = "Português";
+        String genero = "Ação";
+        Double duracao = 60d;
 
         Assert.assertThrows(IllegalArgumentException.class, () -> {
-            Midia midia = new Filme(nome, idioma, genero, duracao);;
+            Midia midia = new Filme(nome, idioma, genero, duracao);
+            ;
         });
 
     }
-    
+
+    @Test
+    public void deveAdicionarUmaAvaliacao() throws IllegalArgumentException, SenhaFracaException {
+        String nome = "Filme A";
+        String idioma = "Português";
+        String genero = "Ação";
+        Double duracao = 60d;
+        Midia midia = new Filme(nome, idioma, genero, duracao);
+
+        Assert.assertEquals(0, midia.getTodasAvaliacoes().size());
+
+        Cliente cliente1 = new Cliente("John Doe", "johndoe", "password123");
+        Avaliacao avaliacao = new Avaliacao(cliente1.getLogin(), 4);
+
+        midia.addAvaliacao(avaliacao);
+
+        Assert.assertEquals(1, midia.getTodasAvaliacoes().size());
+
+    }
+
 }
