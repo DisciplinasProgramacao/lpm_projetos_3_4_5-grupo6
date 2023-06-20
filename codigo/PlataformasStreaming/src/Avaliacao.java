535701e57557982c1261dc5bbcf171486f95d492
Felipe Vieira <luizfelipevieira01@gmail.com>
2023-06-19 21:19:06 -0300

    ajuste classe Avaliacao

--- codigo/PlataformasStreaming/src/Avaliacao.java
+++ codigo/PlataformasStreaming/src/Avaliacao.java
@@ -1,26 +1,87 @@
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
 import java.security.InvalidParameterException;
-import java.security.spec.InvalidParameterSpecException;
 import java.util.Date;
 
-@Data
+/**
+ * 
+ * Representa uma avaliação feita por um usuário.
+ */
 public class Avaliacao {
 
     private String login;
     private Date dataDaAvaliacao;
     private int pontuacao;
 
-    public Avaliacao(String login, int pontuacao) {
-        if (login == null) {
-            throw new InvalidParameterException("Cliente inválido");
-        }
-        if (pontuacao < 1 || pontuacao > 5) {
-            throw new InvalidParameterException("Digite uma nota entre 1 e 5");
-        }
+    /**
+     * 
+     * Constrói um novo objeto Avaliacao com o login e pontuação especificados.
+     * 
+     * @param login     o login do usuário que fez a avaliação
+     * @param pontuacao a pontuação da avaliação (entre 1 e 5)
+     * @throws InvalidParameterException se o login for nulo ou a pontuação estiver
+     *                                   fora do intervalo válido
+     */
+    public Avaliacao(String login, int pontuacao) throws InvalidParameterException {
+        validarLogin(login);
+        validarPontuacao(pontuacao);
         this.login = login;
         this.pontuacao = pontuacao;
         this.dataDaAvaliacao = new Date();
+    }
+
+    /**
+     * 
+     * Valida o parâmetro de login.
+     * 
+     * @param login o login a ser validado
+     * @throws InvalidParameterException se o login for nulo
+     */
+    private void validarLogin(String login) throws InvalidParameterException {
+        if (login == null) {
+            throw new InvalidParameterException("Cliente não pode ser null");
+        }
+    }
+
+    /**
+     * 
+     * Valida o parâmetro de pontuação.
+     * 
+     * @param pontuacao a pontuação a ser validada
+     * @throws InvalidParameterException se a pontuação estiver fora do intervalo
+     *                                   válido
+     */
+    private void validarPontuacao(int pontuacao) throws InvalidParameterException {
+        if (pontuacao < 1 || pontuacao > 5) {
+            throw new InvalidParameterException("A deve ser um valor entre 1 e 5");
+        }
+    }
+
+    /**
+     * 
+     * Obtém o login do usuário que fez a avaliação.
+     * 
+     * @return o login do usuário
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * 
+     * Obtém a data da avaliação.
+     * 
+     * @return a data da avaliação
+     */
+    public Date getDataDaAvaliacao() {
+        return dataDaAvaliacao;
+    }
+
+    /**
+     * 
+     * Obtém a pontuação da avaliação.
+     * 
+     * @return a pontuação da avaliação
+     */
+    public int getPontuacao() {
+        return pontuacao;
     }
 }
--- codigo/PlataformasStreaming/src/MidiaTest.java
+++ codigo/PlataformasStreaming/src/MidiaTest.java
@@ -32,18 +32,5 @@ public class MidiaTest {
         });
 
     }
-    @Test
-    public void testContabilizarMediaDaMidia() {
-        String   nome = "Filme A";
-        String  idioma = "Português";
-        String  genero = "Ação";
-        Double  duracao = 60d;
-        Midia midia = new Filme(nome, idioma, genero, duracao);
-        midia.registrarPontosDeAvaliacoes(2);
-        midia.registrarPontosDeAvaliacoes(5);
-        float avaliacaoEsperada = 3.5f;
-        double margemDeErro = 0.001; 
-        Assert.assertEquals(avaliacaoEsperada, midia.obterMediaDasAvaliacoes(), margemDeErro);
-    }
-
+    
 }
